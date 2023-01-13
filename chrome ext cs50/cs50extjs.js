618d177dec77dc4e80b80854cada588e660f7678
NotKhalid008 <113136121+NotKhalid008@users.noreply.github.com>
2023-01-13 18:39:50 +0100

    Add files via upload

---
+++ chrome ext cs50/cs50extjs.js
@@ -0,0 +1,43 @@
+const myStart = document.querySelector("#myBtn1");
+const myReset = document.getElementById("myBtn2");
+const myPause = document.getElementById("myBtn3");
+const myResume = document.getElementById("myBtn4");
+
+myStart.addEventListener("click", function() {
+ var getNum = document.getElementById("myInpt").value;
+  var myTime = getNum;
+  console.log(myTime)
+  var timer = setInterval (() =>{
+    document.querySelector('#myVal').innerHTML = myTime;
+    myTime--;
+    console.log(myTime)
+    if (myTime < -1) {
+   document.querySelector('#myVal').innerHTML = "done";
+   myAudio = new Audio("cs50audio.mp3");
+  myAudio.play();
+   clearInterval(timer)
+  }
+  }, 1000) 
+
+  myPause.addEventListener("click", function() {
+    document.getElementById("myBtn3").innerHTML = "pause"
+    clearInterval(timer)
+    })
+    myResume.addEventListener("click", function() {
+    timer = setInterval (() =>{
+      document.querySelector('#myVal').innerHTML = myTime;
+      myTime--;
+      if (myTime < -1) {
+        document.querySelector('#myVal').innerHTML = "done";
+        myAudio = new Audio("cs50audio.mp3");
+audio.play();
+        clearInterval(timer)
+       }
+    }, 1000)
+})
+myReset.addEventListener("click", function() {
+    resetBtn = clearInterval(timer)
+    document.querySelector('#myVal').innerHTML = "0000";
+    myAudio.pause();
+  })
+})
